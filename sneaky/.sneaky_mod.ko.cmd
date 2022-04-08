46a7f61692a2ef96ad54a4dd859c49a1053f1263
Wenting Yang <ladadee1031@outlook.com>
2022-04-07 21:21:54 -0400

    rootkit

--- sneaky/.sneaky_mod.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/wy77/sneaky/homework5-kit/sneaky_mod.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000  --build-id  -T ./scripts/module-common.lds -o /home/wy77/sneaky/homework5-kit/sneaky_mod.ko /home/wy77/sneaky/homework5-kit/sneaky_mod.o /home/wy77/sneaky/homework5-kit/sneaky_mod.mod.o;  true
--- sneaky/.sneaky_mod.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/wy77/sneaky/homework5-kit/sneaky_mod.mod := { echo  /home/wy77/sneaky/homework5-kit/sneaky_mod.o;  echo; } > /home/wy77/sneaky/homework5-kit/sneaky_mod.mod
